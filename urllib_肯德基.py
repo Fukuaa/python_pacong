f09b5334ef933dd709ef7bc7b94b7e330b3d8e56
zqf <1936534366@qq.com>
2022-09-23 15:49:06 +0800

    post请求肯德基10页

--- urllib_肯德基.py
+++ urllib_肯德基.py
@@ -1,20 +1,24 @@
+#   http://www.kfc.com.cn/kfccda/ashx/GetStoreList.ashx?op=cname
 import urllib.request
 import urllib.parse
 
+
 def create_request(page):
     data = {
-        "start": (page - 1) * 20,
-        "limit": 20
+        'cname': '深圳',
+        'pid': '',
+        'pageIndex': page,
+        'pageSize': '10'
     }
-    data = urllib.parse.urlencode(data)
-    url = "https://movie.douban.com/j/chart/top_list?type=5&interval_id=100%3A90&action=&" + data
+    data = urllib.parse.urlencode(data).encode("utf-8")
+    url = "http://www.kfc.com.cn/kfccda/ashx/GetStoreList.ashx?op=cname"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/105.0.0.0 Safari/537.36 "
     }
     print(url)
     # 因为参数顺序问题不能直接写url，headers，中间还有data，所以用关键字传参
-    request = urllib.request.Request(url=url, headers=headers)
+    request = urllib.request.Request(url=url, headers=headers,data=data)
     return request
 
 
@@ -26,7 +30,7 @@ def get_content(request):
 
 
 def down_load(page, content):
-    with open("豆瓣电影" + str(page) + ".json", "w", encoding="utf-8") as fp:
+    with open("肯德基地址" + str(page) + ".json", "w", encoding="utf-8") as fp:
         fp.write(content)
 
 
@@ -34,4 +38,4 @@ start_page = int(input("请输入起始页"))
 end_page = int(input("请输入结束页"))
 for page in range(start_page, end_page + 1):
     down_load(page, get_content(create_request(page)))
-    #create_request(page)
+    # create_request(page)
